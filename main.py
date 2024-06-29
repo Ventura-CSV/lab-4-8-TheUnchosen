b3f23529e4e9ae26a025d100a56c5269fce03735
TheUnchosen <longbui369@gmail.com>
2024-06-29 23:53:51 +0000

    121212

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    while True:
+        x = int(input("First integer value greater than 1: "))
+        if x > 1:
+            break
+
+    while True:    
+        y = int(input("Second integer value greater than 1: "))
+        if y > x:
+            break
+
+    while x < y:
+        if x % 2 == 0:
+            plist.append(x)
+        x += 1
+
 
     return plist
 
